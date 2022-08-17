73e3e98b99a133b366e186e389ff2f20ce3c4e65
AJAY <ajayvaghode.logixbuilt@gmail.com>
2022-08-17 16:33:13 +0530

    adminlogin

--- src/main/java/PackageAccessQue/AdmiLoginLIVE.java
+++ src/main/java/PackageAccessQue/AdmiLoginLIVE.java
@@ -46,38 +46,10 @@ public class AdmiLoginLIVE
 		System.out.println(alrtms);
 		myalert.accept();	
 		
-		//mydriver.navigate().refresh();
-								
+		//mydriver.navigate().refresh();								
 		System.out.println("ADMIN LOGIN SUCCESSFULLY");
 						
 		//mydriver.close();
 	}	
 }
-
-	/*
-	 * public void loginTest() {
-	 * 
-	 * Response myresponse = RestAssured.get(
-	 * "https://services.accessque.com/api/v1/templateByName/passwordverification");
-	 * //Response myresponse = (Response) RestAssured.get(
-	 * "https://services.accessque.com/api/v1/templateByName/passwordverification");
-	 * 
-	 * System.out.println(myresponse.getContentType());
-	 * System.out.println(myresponse.toString());
-	 * //System.out.println(myresponse.getBody().asString());
-	 * System.out.println(myresponse.getStatusCode());
-	 * System.out.println(myresponse.toString());
-	 * 
-	 * System.out.println(myresponse.getCookies());
-	 * System.out.println(myresponse.getHeaders());
-	 *  
-	 * System.out.println(myresponse.getHeader("content-type"));
-	 * 
-	 * }
-	 */
-	
-
-
-
-
 
