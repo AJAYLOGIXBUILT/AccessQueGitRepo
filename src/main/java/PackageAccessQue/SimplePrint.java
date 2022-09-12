edd0578784dd27c7bd1a51f424fb6f2fabad9937
AJAY <ajayvaghode.logixbuilt@gmail.com>
2022-09-12 15:30:37 +0530

    new

--- src/main/java/PackageAccessQue/SimplePrint.java
+++ src/main/java/PackageAccessQue/SimplePrint.java
@@ -1,7 +1,11 @@
 package PackageAccessQue;
 
-public class SimplePrint {
+import org.junit.Test;
 
+public class SimplePrint
+{
+
+	@Test
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HI, DEPLOYED CODE ON SERVER !");
