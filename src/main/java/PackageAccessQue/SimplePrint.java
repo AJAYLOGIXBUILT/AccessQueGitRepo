a10da9c6f2748cdbab4fa902a8bd779152d75db5
AJAY <ajayvaghode.logixbuilt@gmail.com>
2022-09-12 10:44:16 +0530

    Simple project

--- src/main/java/PackageAccessQue/SimplePrint.java
+++ src/main/java/PackageAccessQue/SimplePrint.java
@@ -9,3 +9,27 @@ public class SimplePrint {
 	}
 
 }
+
+
+
+/*
+ 
+ <class name="PackageAccessQue.allPagesAPIresponse"/>
+      <class name="PackageAccessQue.campBellsVilleDashboardAPI"/>
+      <class name="PackageAccessQue.TigerNetApplicationForm"/>
+      <class name="PackageAccessQue.fileUploadDocs"/>
+      <class name="PackageAccessQue.multipleTestCases"/>
+      <class name="PackageAccessQue.gradBussinessAPI"/>
+      <class name="PackageAccessQue.RestAssuredAPITesting"/>
+      <class name="PackageAccessQue.simpleclass"/>
+      <class name="PackageAccessQue.TigerNetApplicationForm2"/>
+      <class name="PackageAccessQue.AfterLoginAllAPI2"/>
+      <class name="PackageAccessQue.simple"/>
+      <class name="PackageAccessQue.MyNewSite"/>
+      <class name="PackageAccessQue.SignInAPI"/>
+      <class name="PackageAccessQue.TigerNetApplicationWithoutMethod"/>
+      <class name="PackageAccessQue.LoginTestCases"/>
+      <class name="PackageAccessQue.LoginPageTest"/> 
+  
+ */
+
