01723061b2217746d9f271063d5d751f4c5b4194
AJAY <ajayvaghode.logixbuilt@gmail.com>
2022-09-13 19:56:20 +0530

    testng 6.9

--- src/main/java/PackageAccessQue/SimplePrint.java
+++ src/main/java/PackageAccessQue/SimplePrint.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 public class SimplePrint
 {
 	@Test
-	public static void main(String[] args) throws InterruptedException
+	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		System.out.println("HI,JENKINS AND GIT CODE CONFIGURATIONE ON SERVER OKAY!");
