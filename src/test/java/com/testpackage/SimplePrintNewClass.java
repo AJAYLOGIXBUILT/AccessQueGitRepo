96fe35d0223d6a06c6b5ba538fa7d42e090a67a3
AJAY <ajayvaghode.logixbuilt@gmail.com>
2022-09-13 19:07:40 +0530

    New change

---
+++ src/test/java/com/testpackage/SimplePrintNewClass.java
@@ -0,0 +1,14 @@
+package com.testpackage;
+
+import org.testng.annotations.Test;
+
+public class SimplePrintNewClass 
+{
+	@Test
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("New Package and new class file created by me");
+	}
+
+}
