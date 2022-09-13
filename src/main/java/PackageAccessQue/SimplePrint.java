4638bc4b5211f7409d862fc4a4322e8a8f46e946
AJAY <ajayvaghode.logixbuilt@gmail.com>
2022-09-13 17:44:40 +0530

    new pom

--- src/main/java/PackageAccessQue/SimplePrint.java
+++ src/main/java/PackageAccessQue/SimplePrint.java
@@ -5,7 +5,6 @@ public class SimplePrint
 {
 	@Test
 	public static void main(String[] args) throws InterruptedException
-	//public void mymain() throws InterruptedException
 	{
 		// TODO Auto-generated method stub
 		System.out.println("HI,JENKINS AND GIT CODE CONFIGURATIONE ON SERVER OKAY!");
@@ -16,24 +15,5 @@ public class SimplePrint
 
 
 
-/*
- 
- <class name="PackageAccessQue.allPagesAPIresponse"/>
-      <class name="PackageAccessQue.campBellsVilleDashboardAPI"/>
-      <class name="PackageAccessQue.TigerNetApplicationForm"/>
-      <class name="PackageAccessQue.fileUploadDocs"/>
-      <class name="PackageAccessQue.multipleTestCases"/>
-      <class name="PackageAccessQue.gradBussinessAPI"/>
-      <class name="PackageAccessQue.RestAssuredAPITesting"/>
-      <class name="PackageAccessQue.simpleclass"/>
-      <class name="PackageAccessQue.TigerNetApplicationForm2"/>
-      <class name="PackageAccessQue.AfterLoginAllAPI2"/>
-      <class name="PackageAccessQue.simple"/>
-      <class name="PackageAccessQue.MyNewSite"/>
-      <class name="PackageAccessQue.SignInAPI"/>
-      <class name="PackageAccessQue.TigerNetApplicationWithoutMethod"/>
-      <class name="PackageAccessQue.LoginTestCases"/>
-      <class name="PackageAccessQue.LoginPageTest"/> 
-  
- */
+
 
